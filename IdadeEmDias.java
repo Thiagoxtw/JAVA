eb7dd9d2fc0a640355433bbbb5780446e90b9d05
Thiagoxtw <devthiago37@gmail.com>
2025-01-17 17:31:18 -0300

    calculadora finalizada

---
+++ IdadeEmDias.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class IdadeEmDias {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int ano = 365; 
+        int mes = 30;  
+        int total;
+
+        System.out.print("Insira sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Insira o número do mês em que nasceu (1 a 12): ");
+        int mesNascimento = scanner.nextInt();
+
+       
+        total = idade * ano + (mesNascimento * mes);
+
+        
+        System.out.println("Sua idade em dias é aproximadamente: " + total + " dias.");
+    }
+}
+
